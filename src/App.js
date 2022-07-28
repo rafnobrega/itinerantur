a44f1eecda1952e3297cadc95c6c846305631606
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-07-28 11:45:15 -0700

    fix issues with image imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import "./App.css";
-import Home from "./components/pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Services from "./components/pages/Services";
-import Products from "./components/pages/Products";
-import SignUp from "./components/pages/SignUp";
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/services" component={Services} />
-          <Route path="/products" component={Products} />
-          <Route path="/sign-up" component={SignUp} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </>
